1a2afdc2f78cfaf26cb225180965d1b933d6314c
Tarequ Lateef <146071062+TarequLateef@users.noreply.github.com>
2024-07-02 08:31:04 +0300

    Delete ElwadyFingerPirntFo_Blazor directory

--- ElwadyFingerPirntFo_Blazor/Services/EmpSchema/Interfaces/ICertService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ElwadyFingerPrint.Core.Models.EmpSchema;
-using EmpSchema;
-
-namespace ElwadyFingerPirntFo_Blazor.Services.EmpSchema.Interfaces
-{
-    public interface ICertService:IServiceRepository<CertificateTbl>
-    {
-    }
-}
--- ElwadyFingerPirntFo_Blazor/Services/EmpSchema/Interfaces/IEmpService.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmpSchema;
-
-namespace ElwadyFingerPirntFo_Blazor.Services.EmpSchema.Interfaces
-{
-    public interface IEmpService :IServiceRepository<Employees>
-    {
-        
-    }
-}
